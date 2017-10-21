5e28f691fd35e0f80d018ae64871686abe811d4a
raider <rider@ArchRaider.numericable.fr>
2017-10-21 20:59:59 +0200

    updated script

--- script.sql
+++ script.sql
@@ -1,4 +1,4 @@
-/*All User's are stored in APP_USER table*/
+/*All User's arAPP_USER in APP_USER table*/
 create table APP_USER (
    id BIGINT NOT NULL AUTO_INCREMENT,
    sso_id VARCHAR(30) NOT NULL,
@@ -22,10 +22,10 @@ create table USER_PROFILE(
 /* JOIN TABLE for MANY-TO-MANY relationship*/ 
 CREATE TABLE APP_USER_USER_PROFILE (
     user_id BIGINT NOT NULL,
-    user_profile_id BIGINT NOT NULL,
-    PRIMARY KEY (user_id, user_profile_id),
+    USER_PROFILE_id BIGINT NOT NULL,
+    PRIMARY KEY (user_id, USER_PROFILE_id),
     CONSTRAINT FK_APP_USER FOREIGN KEY (user_id) REFERENCES APP_USER (id),
-    CONSTRAINT FK_USER_PROFILE FOREIGN KEY (user_profile_id) REFERENCES USER_PROFILE (id)
+    CONSTRAINT FK_USER_PROFILE FOREIGN KEY (USER_PROFILE_id) REFERENCES USER_PROFILE (id)
 );
  
 /* Populate USER_PROFILE Table */
@@ -55,26 +55,26 @@ INSERT INTO APP_USER(sso_id, password, first_name, last_name, email, state)
 VALUES ('kenny','abc127', 'Kenny','Roger','kenny@xyz.com', 'Active');
  
 /* Populate JOIN Table */
-INSERT INTO APP_USER_USER_PROFILE (user_id, user_profile_id)
-  SELECT user.id, profile.id FROM app_user user, user_profile profile  
+INSERT INTO APP_USER_USER_PROFILE (user_id, USER_PROFILE_id)
+  SELECT user.id, profile.id FROM APP_USER user, USER_PROFILE profile  
   where user.sso_id='bill' and profile.type='USER';
  
-INSERT INTO APP_USER_USER_PROFILE (user_id, user_profile_id)
-  SELECT user.id, profile.id FROM app_user user, user_profile profile
+INSERT INTO APP_USER_USER_PROFILE (user_id, USER_PROFILE_id)
+  SELECT user.id, profile.id FROM APP_USER user, USER_PROFILE profile
   where user.sso_id='danny' and profile.type='USER';
  
-INSERT INTO APP_USER_USER_PROFILE (user_id, user_profile_id)
-  SELECT user.id, profile.id FROM app_user user, user_profile profile
+INSERT INTO APP_USER_USER_PROFILE (user_id, USER_PROFILE_id)
+  SELECT user.id, profile.id FROM APP_USER user, USER_PROFILE profile
   where user.sso_id='sam' and profile.type='ADMIN';
  
-INSERT INTO APP_USER_USER_PROFILE (user_id, user_profile_id)
-  SELECT user.id, profile.id FROM app_user user, user_profile profile
+INSERT INTO APP_USER_USER_PROFILE (user_id, USER_PROFILE_id)
+  SELECT user.id, profile.id FROM APP_USER user, USER_PROFILE profile
   where user.sso_id='nicole' and profile.type='DBA';
  
-INSERT INTO APP_USER_USER_PROFILE (user_id, user_profile_id)
-  SELECT user.id, profile.id FROM app_user user, user_profile profile  
+INSERT INTO APP_USER_USER_PROFILE (user_id, USER_PROFILE_id)
+  SELECT user.id, profile.id FROM APP_USER user, USER_PROFILE profile  
   where user.sso_id='kenny' and profile.type='ADMIN';
  
-INSERT INTO APP_USER_USER_PROFILE (user_id, user_profile_id)
-  SELECT user.id, profile.id FROM app_user user, user_profile profile  
+INSERT INTO APP_USER_USER_PROFILE (user_id, USER_PROFILE_id)
+  SELECT user.id, profile.id FROM APP_USER user, USER_PROFILE profile  
   where user.sso_id='kenny' and profile.type='DBA';
